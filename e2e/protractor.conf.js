07410cdef3287636f98acdee8d20bc6a91974c86
Patrick <pmoore4181@gmail.com>
2019-11-25 13:48:02 -0600

    added chromeOptions to protactor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,3 @@
-// @ts-check
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 /**
@@ -13,7 +9,14 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    browserName: 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': [
+        '--no-sandbox',
+        '--headless',
+        '--window-size=1024,768'
+      ]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
